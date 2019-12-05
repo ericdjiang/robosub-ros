275090d61c07564e0cfdf764232a176fb570b81e
hkder <37953030+hkder@users.noreply.github.com>
2019-12-05 16:36:57 -0500

    Update IMU.py

--- src/data_pub/scripts/IMU.py
+++ src/data_pub/scripts/IMU.py
@@ -4,24 +4,47 @@ import numpy as np
 import math
 import serial
 import time
+import serial.tools.list_ports as list_ports
 
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import Imu, MagneticField
 from tf.transformations import euler_from_quaternion
 
-IMU_DEST_TOPIC_RPY = 'sensors/imu/rpy'
-IMU_DEST_TOPIC_MAG = 'sensors/imu/mag'
-IMU_DEST_TOPIC_ACCEL = 'sensors/imu/accel'
-IMU_DEST_TOPIC_GYRO = 'sensors/imu/gyro'
-IMU_DEST_TOPIC_QUAT = 'sensors/imu/quat'
-NODE_NAME = 'imu_pub'
+class IMURawPublisher:
 
-ser = serial.Serial("COM11", 115200)
-ser.close()
-ser.open()
+    IMU_DEST_TOPIC_RPY = 'sensors/imu/rpy'
+    IMU_DEST_TOPIC_MAG = 'sensors/imu/mag'
+    IMU_DEST_TOPIC_ACCEL = 'sensors/imu/accel'
+    IMU_DEST_TOPIC_GYRO = 'sensors/imu/gyro'
+    IMU_DEST_TOPIC_QUAT = 'sensors/imu/quat'
+
+    FTDI_STR = 'FT232R'
+    BAUDRATE = 115200
+    NODE_NAME = 'imu_pub'
+
+    def __init__(self):
+        self._pub_imu = rospy.Publisher(self.IMU_DEST_TOPIC_QUAT, Imu, queue_size=50)
+        self._pub_mag = rospy.Publisher(self.IMU_DEST_TOPIC_MAG, MagneticField, queue_size=50)
+
+        self._current_imu_msg = Imu()
+        self._current_mag_msg = MagneticField()
+
+        self._serial_port = None
+        self._serial = None
+
+    def run(self):
+        rospy.init_node(self.NODE_NAME)
+
+        self._serial_port = next(list_ports.grep(FTDI_STR)).device
+        self._serial = serial.Serial(_serial_port, BAUDRATE, timeout=0.1, write_timeout=1.0,
+                                     bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                                     stopbits=serial.STOPBITS_ONE)
+
+        while not rospy.is_shutdown():
+
+
 
 def talker():
-    rospy.init_node(NODE_NAME)
     imu_pub_rpy = rospy.Publisher(IMU_DEST_TOPIC_RPY, Vector3, queue_size=50)
     imu_pub_mag = rospy.Publisher(IMU_DEST_TOPIC_MAG, Vector3, queue_size=50)
     imu_pub_accel = rospy.Publisher(IMU_DEST_TOPIC_ACCEL, Vector3, queue_size=50)
@@ -34,41 +57,33 @@ def talker():
     imu_msg.header.frame_id = "imu_link"
     mag_msg.header.frame_id = "mag_link"
 
+    _serial_port = next(list_ports.grep(FTDI_STR)).device
+    _serial = serial.Serial(_serial_port, BAUDRATE, timeout=0.1, write_timeout=1.0,
+                            bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
+                            stopbits=serial.STOPBITS_ONE)
+
     try:
-        while(1):
-            line = str(ser.readline())
-            items = line.split(",")
-            imu_pub_data.header.stamp = rospy.Time.now()
-            imu_pub_data.header.stamp = rospy.Time.now()
+        line = str(self._serial.readline())
+        items = line.split(",")
+        imu_pub_data.header.stamp = rospy.Time.now()
+        imu_pub_data.header.stamp = rospy.Time.now()
 
-            imu_msg.orientation.w = items[4]
-            imu_msg.orientation.x = items[1]
-            imu_msg.orientation.y = items[2]
-            imu_msg.orientation.z = items[3]
+        imu_msg.orientation.w = items[4]
+        imu_msg.orientation.x = items[1]
+        imu_msg.orientation.y = items[2]
+        imu_msg.orientation.z = items[3]
 
-            imu_msg.linear_acceleration.x = items[8]
-            imu_msg.linear_acceleration.y = items[9]
-            imu_msg.linear_acceleration.z = items[10]
-            imu_msg.linear_acceleration_covariance[0] = -1
+        imu_msg.linear_acceleration.x = items[8]
+        imu_msg.linear_acceleration.y = items[9]
+        imu_msg.linear_acceleration.z = items[10]
+        imu_msg.linear_acceleration_covariance[0] = -1
 
-            imu_msg.angular_velocity.x = items[11]
-            imu_msg.angular_velocity.y = items[12]
-            imu_msg.angular_velocity.z = items[13]
-            imu_msg.angular_velocity_covariance[0] = -1
-
-            imu_pub_data.publish(imu_msg)
-
-            mag_msg.magnetic_field.x = items[5]
-            mag_msg.magnetic_field.y = items[6]
-            mag_msg.magnetic_field.z = items[7]
-            
-            mag_pub_data.publish(mag_msg)
-
-            #print("yaw:\t" + str(float(items[1])) + "\t\t pitch: \t" + str(float(items[2])) + "\t roll: \t" + str(float(items[3])))
-            #time.sleep(0.5)
-    except:
-        ser.close()
-        exit()
-
+        imu_msg.angular_velocity.x = items[11]
+        imu_msg.angular_velocity.y = items[12]
+        imu_msg.angular_velocity.z = items[13]
+        imu_msg.angular_velocity_covariance[0] = -1
 if __name__ == '__main__':
-    talker()
+    try:
+        IMURawPublisher.run()
+    except rospy.ROSInterruptException:
+        pass
