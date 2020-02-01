ced5437887e4456fa5954a1a8e46487a905facfb
bguo919 <56141027+bguo919@users.noreply.github.com>
2020-02-01 13:06:22 -0500

    Update joystick.py

--- src/data_pub/scripts/joystick.py
+++ src/data_pub/scripts/joystick.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#import rospy
+import rospy
 import numpy as np
 import XInput as xin
 import math
@@ -8,33 +8,7 @@ import sys
 
 from geometry_msgs.msg import Twist
 
-#class Joystick:
-    
-<<<<<<< HEAD
-#    NODE_NAME = 'joy_pub'
-#    LISTENING_TOPIC = '
-    
-#    def __init__(self):
-#        self._pub_imu = rospy.Publisher(self.IMU_DEST_TOPIC_QUAT, IMU, queue_size=50)
-        
-
-    
-#if __name__ == '__main__':
-#try:
-#    IMURawPublisher().run()
-#except rospy.ROSInterruptException:
-#    pass
-
-print("Hello World!")
-
-#try:
-#	while(1):
-#		hold = xin.get_thumb_values(xin.get_state(0))
-#		print(hold)
-#except:
-#	exit()
-
-=======
+class Joystick:
     NODE_NAME = 'joy_pub'
     JOY_DEST_TOPIC_LOCAL = 'controls/desired_twist_local'
     JOY_DEST_TOPIC_GLOBAL = 'controls/desired_twist_global'
@@ -57,4 +31,3 @@ if __name__ == '__main__':
         IMURawPublisher().run()
     except rospy.ROSInterruptException:
         pass
->>>>>>> 89eb99d8bfad43cfe60246c74d7e0d9a55f9b2c4
