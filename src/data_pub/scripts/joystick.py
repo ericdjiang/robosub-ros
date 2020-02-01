0a8453aad5878b1bfec70df41a00ec49c591ce86
Kevin Yang <kevin.yang@duke.edu>
2020-02-01 12:32:56 -0500

    added f310 core code

--- src/data_pub/scripts/joystick.py
+++ src/data_pub/scripts/joystick.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import rospy
+#import rospy
 import numpy as np
 import XInput as xin
 import math
@@ -7,18 +7,28 @@ import time
 import sys
 
 
-class Joystick:
+#class Joystick:
     
-    NODE_NAME = 'joy_pub'
-    LISTENING_TOPIC = '
+#    NODE_NAME = 'joy_pub'
+#    LISTENING_TOPIC = '
     
-    def __init__(self):
-        self._pub_imu = rospy.Publisher(self.IMU_DEST_TOPIC_QUAT, IMU, queue_size=50)
+#    def __init__(self):
+#        self._pub_imu = rospy.Publisher(self.IMU_DEST_TOPIC_QUAT, IMU, queue_size=50)
         
 
     
-if __name__ == '__main__':
-try:
-    IMURawPublisher().run()
-except rospy.ROSInterruptException:
-    pass
+#if __name__ == '__main__':
+#try:
+#    IMURawPublisher().run()
+#except rospy.ROSInterruptException:
+#    pass
+
+print("Hello World!")
+
+#try:
+#	while(1):
+#		hold = xin.get_thumb_values(xin.get_state(0))
+#		print(hold)
+#except:
+#	exit()
+
