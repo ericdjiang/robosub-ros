d3ea4d6a24b2bfe0ae5b2f92bed7cd2ce2d70d8d
bguo919 <56141027+bguo919@users.noreply.github.com>
2020-02-04 21:15:23 -0500

    Update IMU.py

--- src/data_pub/scripts/IMU.py
+++ src/data_pub/scripts/IMU.py
@@ -11,13 +11,10 @@ from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import Imu, MagneticField
 from tf.transformations import euler_from_quaternion
 
-
 class IMURawPublisher:
-    IMU_DEST_TOPIC_RPY = 'sensors/imu/rpy'
-    IMU_DEST_TOPIC_MAG = 'sensors/imu/mag'
-    IMU_DEST_TOPIC_ACCEL = 'sensors/imu/accel'
-    IMU_DEST_TOPIC_GYRO = 'sensors/imu/gyro'
+
     IMU_DEST_TOPIC_QUAT = 'sensors/imu/quat'
+    IMU_DEST_TOPIC_MAG = 'sensors/imu/mag'
 
     FTDI_STR = 'FT1WDFQ2'
     BAUDRATE = 115200
@@ -26,10 +23,7 @@ class IMURawPublisher:
 
     def __init__(self):
         self._pub_imu = rospy.Publisher(self.IMU_DEST_TOPIC_QUAT, Imu, queue_size=50)
-        self._pub_imu_rpy = rospy.Publisher(self.IMU_DEST_TOPIC_RPY, Vector3, queue_size=50)
-        self._pub_imu_mag = rospy.Publisher(self.IMU_DEST_TOPIC_MAG, MagneticField, queue_size=50)
-        self._pub_imu_accel = rospy.Publisher(self.IMU_DEST_TOPIC_ACCEL, Vector3, queue_size=50)
-        self._pub_imu_gyro = rospy.Publisher(self.IMU_DEST_TOPIC_GYRO, Vector3, queue_size=50)
+        self._pub_mag = rospy.Publisher(self.IMU_DEST_TOPIC_MAG, MagneticField, queue_size=50)
 
         self._current_imu_msg = Imu()
         self._current_mag_msg = MagneticField()
@@ -42,19 +36,19 @@ class IMURawPublisher:
     def run(self):
         rospy.init_node(self.NODE_NAME)
         self._serial_port = next(list_ports.grep(self.FTDI_STR)).device
-	self._serial = serial.Serial(self._serial_port, self.BAUDRATE, timeout=None, write_timeout=None, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+		self._serial = serial.Serial(self._serial_port, self.BAUDRATE, timeout=None, write_timeout=None, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
         
         while not rospy.is_shutdown():
-	    line = self._serial.read_until()	
-            self._pub_imu.header.stamp = rospy.Time.now()
-            items = self._extract_line(line)  # array of line
-	    if items[0] == "$VNQMR":
-	        rospy.loginfo(line)
-                self._parse_orient(items)
-                self._parse_accel(items)
-                self._parse_angvel(items)
-	        self._parse_mag(items)
-                self._publish_current_msg()
+			line = self._serial.read_until()	
+			self._pub_imu.header.stamp = rospy.Time.now()
+			items = self._extract_line(line)  # array of line
+			if items[0] == "$VNQMR":
+				rospy.loginfo(line)
+				self._parse_orient(items)
+				self._parse_accel(items)
+				self._parse_angvel(items)
+				self._parse_mag(items)
+				self._publish_current_msg()
 
     def _extract_line(self, line):
         return line.split(self.LINE_DELIM)
@@ -64,7 +58,7 @@ class IMURawPublisher:
         self._current_imu_msg.orientation.y = float(items[2])
         self._current_imu_msg.orientation.z = float(items[3])
         self._current_imu_msg.orientation.w = float(items[4])
-	self._current_imu_msg.orientation_covariance[0] = -1
+		self._current_imu_msg.orientation_covariance[0] = -1
 
     def _parse_accel(self, items):
         self._current_imu_msg.linear_acceleration.x = float(items[8])
@@ -75,21 +69,21 @@ class IMURawPublisher:
     def _parse_angvel(self, items):
         self._current_imu_msg.angular_velocity.x = float(items[11])
         self._current_imu_msg.angular_velocity.y = float(items[12])
-	items[13] = items[13][0:10]
+		items[13] = items[13][0:10]
         self._current_imu_msg.angular_velocity.z = float(items[13])
         self._current_imu_msg.angular_velocity_covariance[0] = -1
 
     def _parse_mag(self, items):
-	self._current_mag_msg.magnetic_field.x = float(items[5])
-	self._current_mag_msg.magnetic_field.y = float(items[6])
-	self._current_mag_msg.magnetic_field.z = float(items[7])
-	self._current_mag_msg.magnetic_field_covariance[0] = -1
+		self._current_mag_msg.magnetic_field.x = float(items[5])
+		self._current_mag_msg.magnetic_field.y = float(items[6])
+		self._current_mag_msg.magnetic_field.z = float(items[7])
+		self._current_mag_msg.magnetic_field_covariance[0] = -1
 
     def _publish_current_msg(self):
         self._pub_imu.publish(self._current_imu_msg)
         self._current_imu_msg = Imu()
-	self._pub_imu_mag.publish(self._current_mag_msg)
-	self._current_mag_msg = MagneticField()
+		self._pub_mag.publish(self._current_mag_msg)
+		self._current_mag_msg = MagneticField()
 
 if __name__ == '__main__':
     try:
